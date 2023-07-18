6fc5e6b6543fe8afd7dcc8a63f9d46838f52825f
ikuto0608 <ikuto0608@me.com>
2023-07-18 15:16:52 +0900

    coprocessor behavior reproduction

--- coprocessors/node/index.js
+++ coprocessors/node/index.js
@@ -1,16 +1,4 @@
-const { randomUUID: uuid } = require('crypto')
 const express = require('express')
-const jwt = require('jsonwebtoken')
-
-const JWT_SECRET = process.env.JWT_SECRET || 'apollo'
-
-function handleCreateJwt(_req, res) {
-  const payload = {
-    client_name: req.data.clientName || 'node coprocessor',
-    client_version: req.data.clientVersion || 1,
-  }
-  res.json({ token: jwt.sign(payload, JWT_SECRET) })
-}
 
 function getRequestPayload(req) {
   const payload = req.body
@@ -21,78 +9,55 @@ function getRequestPayload(req) {
   return payload
 }
 
-function sendUnauthenticated(res, body) {
-  res.json({
+function stringifiedValidJson(res, body) {
+  return res.json({
     ...body,
-    control: { break: 401 }
+    control: { break: 401 },
+    body: JSON.stringify({
+      errors: [
+        {
+          message: 'Not authenticated.',
+          extensions: {
+            code: 'ERR_UNAUTHENTICATED',
+          },
+        },
+      ],
+    })
   })
-
-  return
 }
 
-function handleClientAwareness(req, res) {
-  if (req.body.stage !== 'RouterRequest') {
-    res.json(req.body)
-    return
-  }
-
+function handleValidStringifiedJson(req, res) {
   const payload = getRequestPayload(req)
-  if (!payload.headers['authentication']) {
-    return sendUnauthenticated(res, payload)
-  }
-
-  const token = payload.headers['authentication'][0].split('Bearer ')[1]
-  if (!token) {
-    return sendUnauthenticated(res, payload)
-  }
-
-  try {
-    const jwtPayload = jwt.verify(token, JWT_SECRET)
-    payload.headers['apollographql-client-name'] = [jwtPayload.client_name || 'coprocessor']
-    payload.headers['apollographql-client-version'] = [jwtPayload.client_version || 'loadtest']
-
-    res.json(payload)
-  } catch {
-    sendUnauthenticated(res, payload)
-  }
+  return stringifiedValidJson(res, payload)
 }
 
-function handleGuidResponse(req, res) {
-  if (req.body.stage !== 'RouterResponse') {
-    res.json(req.body)
-    return
-  }
-
-  const payload = getRequestPayload(req)
-
-  payload.headers['GUID'] = []
-  for (let i = 0; i < 10; i++) {
-    payload.headers['GUID'].push(uuid())
-  }
-
-  res.json(payload)
+function validJson(res, body) {
+  return res.json({
+    ...body,
+    control: { break: 401 },
+    body: {
+      errors: [
+        {
+          message: 'Not authenticated.',
+          extensions: {
+            code: 'ERR_UNAUTHENTICATED',
+          },
+        },
+      ],
+    }
+  })
 }
 
-function handleStaticSubgraph(req, res) {
-  if (req.body.stage !== 'SubgraphRequest') {
-    res.json(req.body)
-    return
-  }
-
+function handleValidJson(req, res) {
   const payload = getRequestPayload(req)
-
-  payload.headers['source'] = ['coprocessor']
-
-  res.json(payload)
+  return validJson(res, payload)
 }
 
 const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
-app.post('/create-jwt', handleCreateJwt)
-app.post('/client-awareness', handleClientAwareness)
-app.post('/guid-response', handleGuidResponse)
-app.post('/static-subgraph', handleStaticSubgraph)
+app.post('/stringified-valid-json', handleValidStringifiedJson)
+app.post('/valid-json', handleValidJson)
 app.listen(port, () => {
   console.log(`🚀 Coprocessor running on port ${port}`)
 })
